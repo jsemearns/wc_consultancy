8924ba59e7b30269035438515ba7c00709a46632
jsemearns <justin.mearns@gmail.com>
2017-01-15 10:43:29 +0800

    minor updates

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: python /app/manage.py runserver 0.0.0.0:$PORT
 release: python /app/manage.py migrate
+web: gunicorn app/wcconsultancy/wsgi.py --log-file -
--- wcconsultancy/settings.py
+++ wcconsultancy/settings.py
@@ -134,9 +134,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# import dj_database_url
-# DATABASES['default'] =  dj_database_url.config()
-#
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
 try:
     from local_settings import *
 except ImportError:
