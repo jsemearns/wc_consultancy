e27889882f0ff4a386506e22fba2363f26420111
jsemearns <justin.mearns@gmail.com>
2017-02-10 21:30:02 +0800

    added zepto js, additional fixtures

--- main/models/state.py
+++ main/models/state.py
@@ -4,3 +4,6 @@ from django.db import models
 
 class State(models.Model):
     name = models.CharField(max_length=255)
+
+    def __unicode__(self):
+        return self.name
---
+++ main/static/main/js/university/university_list.js
@@ -0,0 +1,5 @@
+// Zepto('.university-item').each(function(i, item) {
+//     Zepto(item).fadeOut(0, function() {
+//         this.fadeIn(200);
+//     });
+// });
