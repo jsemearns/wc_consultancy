7d6fdf2a8308cd58f77937a2c71c98456408dfcd
jsemearns <justin.mearns@gmail.com>
2017-03-08 21:17:45 +0800

    updated about us page

--- main/static/main/materialize/js/init.js
+++ main/static/main/materialize/js/init.js
@@ -5,5 +5,7 @@
     $('.parallax').parallax();
     $('.carousel.carousel-slider').carousel({full_width: true});
     $('.slider').slider({indicators: false});
+    $('.modal').modal();
+    
   }); // end of document ready
 })(jQuery); // end of jQuery name space
